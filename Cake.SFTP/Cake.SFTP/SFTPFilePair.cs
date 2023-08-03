b9f5b82b71378bb3f7681890cb9826c6e5a479e2
Jeroen Vandezande <Jeroen.Vandezande@gmail.com>
2023-08-03 11:07:52 +0200

    finished FilePair class and added overloaded contructors for Settings class

--- Cake.SFTP/Cake.SFTP/SFTPFilePair.cs
+++ Cake.SFTP/Cake.SFTP/SFTPFilePair.cs
@@ -1,6 +1,36 @@
 namespace Cake.SFTP;
 
+/// <summary>
+/// this class contains a pair of file paths for both the local as the remote path for that file.
+/// </summary>
 public class SFTPFilePair
 {
-    
+    /// <summary>
+    /// The constructor
+    /// </summary>
+    /// <param name="localFilePath">The local path of the file</param>
+    /// <param name="remoteFilePath">The SFTP server path of the file</param>
+    public SFTPFilePair(string localFilePath, string remoteFilePath)
+    {
+        LocalFilePath = localFilePath;
+        RemoteFilePath = remoteFilePath;
+    }
+
+    /// <summary>
+    /// The constructor
+    /// </summary>
+    public SFTPFilePair()
+    {
+        LocalFilePath = String.Empty;
+        RemoteFilePath = String.Empty;
+    }
+
+    /// <summary>
+    /// The local path of the file
+    /// </summary>
+    public String LocalFilePath { get; set; }
+    /// <summary>
+    /// The SFTP server path of the file
+    /// </summary>
+    public String RemoteFilePath { get; set; }
 }
--- Cake.SFTP/Cake.SFTP/SFTPSettings.cs
+++ Cake.SFTP/Cake.SFTP/SFTPSettings.cs
@@ -8,20 +8,60 @@ namespace Cake.SFTP;
 public class SFTPSettings: ToolSettings
 {
     /// <summary>
-    /// The SFTP URL
+    /// The constructor
     /// </summary>
-    public string Host { get; set; } = string.Empty;
+    /// <param name="host">The SFTP Server URL</param>
+    /// <param name="port">The port of the SFTP Server, is normally 22</param>
+    /// <param name="userName">The User Name on the server</param>
+    /// <param name="password">The Password on the server</param>
+    public SFTPSettings(string host, int port, string userName, string password)
+    {
+        Host = host;
+        Port = port;
+        UserName = userName;
+        Password = password;
+    }
+    
+    /// <summary>
+    /// The constructor
+    /// </summary>
+    /// <param name="host">The SFTP Server URL</param>
+    /// <param name="userName">The User Name on the server</param>
+    /// <param name="password">The Password on the server</param>
+    public SFTPSettings(string host, string userName, string password)
+    {
+        Host = host;
+        Port = 22;
+        UserName = userName;
+        Password = password;
+    }
+    
+    /// <summary>
+    /// The constructor
+    /// </summary>
+    public SFTPSettings()
+    {
+        Host = String.Empty;
+        Port = 22;
+        UserName = String.Empty;
+        Password = String.Empty;
+    }
+
+    /// <summary>
+    /// The SFTP Server URL
+    /// </summary>
+    public string Host { get; set; }
 
     /// <summary>
     /// The port of the SFTP Server, is 22 by default
     /// </summary>
-    public int Port { get; set; } = 22;
+    public int Port { get; set; }
     /// <summary>
     /// The User Name on the server
     /// </summary>
-    public string UserName { get; set; } = string.Empty;
+    public string UserName { get; set; }
     /// <summary>
     /// The Password on the server
     /// </summary>
-    public string Password { get; set; } = string.Empty;
+    public string Password { get; set; }
 }
